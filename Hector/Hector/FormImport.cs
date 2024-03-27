23f19ba0f740d44d926f3c52be0db40774dcf217
Antoine Beunas <beunas.antoine@orange.fr>
2024-03-27 15:58:13 +0100

    Changement fichier importation

--- Hector/Hector/FormImport.cs
+++ Hector/Hector/FormImport.cs
@@ -1,13 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.IO;
 using System.Linq;
+using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.LinkLabel;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace Hector
@@ -63,6 +66,8 @@ namespace Hector
         /// <param name="Args">Objet <b>EventArgs</b> contient les informations sur l'évènement.</param>
         private void EcraserDonnees_Click(object Sender, EventArgs Args)
         {
+            // On instancie la variable contenant le nombre d'erreurs de l'importation
+            int NombreErreurs = 0;
             // On regarde si un fichier a été choisi au préalable.
             if (!string.IsNullOrEmpty(_CheminFichier))
             {
@@ -76,20 +81,65 @@ namespace Hector
                 // On lance la lecture du fichier
                 using (StreamReader Sr = File.OpenText(_CheminFichier))
                 {
-                    string Lignes = Sr.ReadToEnd();
-                    // On supprime les espaces en trop avant et après les ";"
-                    Lignes = Lignes.Replace("; ", ";");
-                    Lignes = Lignes.Replace(" ;", ";");
+                    // On s'occupe de connaitre le nombre de tables de la BDD
+                    int NombreColonnes = 0;
+                    
+                    if (!Sr.EndOfStream)
+                    {
+                        string Ligne1 = Sr.ReadLine();
+                        // On supprime les espaces en trop après les ";"
+                        while (Ligne1.Contains("; "))
+                        {
+                            Ligne1 = Ligne1.Replace("; ", ";");
+                        }
+                        // On supprime les espaces en trop avant les ";"
+                        while (Ligne1.Contains(" ;"))
+                        {
+                            Ligne1 = Ligne1.Replace(" ;", ";");
+                        }
+
+                        // On sépare les colonnes via un tableau en omettant les espaces/cases vides.
+                        char[] Separateur = { ';' };
+                        string[] Mots1 = Ligne1.Split(Separateur, StringSplitOptions.RemoveEmptyEntries);
+                        // On garde le nombre de colonnes du tableau de côté
+                        // (pour éviter les erreurs type "pas assez de données" ou "trop de données dans cette ligne")
+                        NombreColonnes = Mots1.Length;
+                    }
+
+                    while (!Sr.EndOfStream)
+                    {
+                        string Ligne = Sr.ReadLine();
+
+                        // On supprime les espaces en trop après les ";"
+                        while (Ligne.Contains("; "))
+                        {
+                            Ligne = Ligne.Replace("; ", ";");
+                        }
+                        // On supprime les espaces en trop avant les ";"
+                        while (Ligne.Contains(" ;"))
+                        {
+                            Ligne = Ligne.Replace(" ;", ";");
+                        }
+
+                        // On sépare les colonnes via un tableau en omettant les espaces/cases vides.
+                        char[] Separateur = { ';' };
+                        string[] Mots = Ligne.Split(Separateur, StringSplitOptions.RemoveEmptyEntries);
+
+                        // On regarde si il y a bien le bon nombre d'informations / de colonnes.
+                        if (Mots.Length == NombreColonnes)
+                        {
+
+                        }
+
+                        else
+                        {
+                            NombreErreurs++;
+                        }
+                    }
 
                     Sr.Close();
                     Sr.Dispose();
 
-                    // Une fois le fichier complètement parsé, on va prélever une par une les lignes
-                    // et on les ajoutent à la base de données.
-                    for (int NbLignes = 0; NbLignes < NombreLignes; NbLignes++)
-                    {
-                        //********** Modifier **********//
-                    }
                 }
             }
         }
